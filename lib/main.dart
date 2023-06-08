587934a7f48e4ac834e405fe33c7d9ed6dd978d0
Johannes Brückl <johannesbrueckl@users.noreply.github.com>
2023-06-08 03:53:14 +0200

    initial desktop

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,5 @@ import 'package:flutter/material.dart';
 import 'package:trinkinator/features/main_menu.dart';
 
 void main() {
-  runApp(MaterialApp(home: MainMenu()));
+  runApp(const MaterialApp(home: MainMenu()));
 }
