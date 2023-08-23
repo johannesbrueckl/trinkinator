acdf0a098c5fe1f2dfeee9471739f3fa033804a0
Johannes Brückl <johannesbrueckl@users.noreply.github.com>
2023-08-23 04:19:56 +0200

    added setState to the game. now direct reload of the screen is possible without poping and reopening it.

--- lib/components/game.dart
+++ lib/components/game.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
 
-class StartGame extends StatelessWidget {
+class StartGame extends StatefulWidget {
   var j;
   var i;
   var aufgaben;
@@ -17,6 +17,11 @@ class StartGame extends StatelessWidget {
       this.spieleranzahl});
 
   @override
+  State<StartGame> createState() => _StartGameState();
+}
+
+class _StartGameState extends State<StartGame> {
+  @override
   Widget build(BuildContext context) {
     Random random = Random();
     return Scaffold(
@@ -25,28 +30,20 @@ class StartGame extends StatelessWidget {
                 shrinkWrap: true,
                 padding: const EdgeInsets.all(20.0),
                 children: [
-          Text.rich(
-            TextSpan(
-              children: <TextSpan>[
-                TextSpan(
-                    text: '${namen[j]}\n\n',
-                    style: const TextStyle(
-                        fontSize: 35.0,
-                        color: Colors.deepOrangeAccent,
-                        fontWeight: FontWeight.bold)),
-                TextSpan(
-                    text:
-                        '${aufgaben[random.nextInt(34)]}\n', //Anzahl der Aufgaben.
-                    style:
-                        const TextStyle(fontSize: 30.0, color: Colors.black)),
-              ],
-            ),
-            textAlign: TextAlign.center,
-            style: const TextStyle(fontSize: 20.0, fontFamily: 'Karla'),
-          ),
+          displayAufgabe(),
           TextButton(
             onPressed: () {
-              Navigator.pop(context);
+              if (widget.j == widget.spieleranzahl) {
+                setState(() {
+                  widget.j = 0;
+                });
+              } else {
+                setState(() {
+                  ++widget.j;
+                });
+              }
+
+              //Navigator.pop(context);
               //return StartGame(context, j, i, aufgaben, namen, spieleranzahl);
             },
             style: TextButton.styleFrom(
@@ -58,6 +55,28 @@ class StartGame extends StatelessWidget {
             child: const Text('Naechste Runde'),
           )
         ])));
+  }
+
+  Text displayAufgabe() {
+    Random random = Random();
+    return Text.rich(
+      TextSpan(
+        children: <TextSpan>[
+          TextSpan(
+              text: '${widget.namen[widget.j]}\n\n',
+              style: const TextStyle(
+                  fontSize: 35.0,
+                  color: Colors.deepOrangeAccent,
+                  fontWeight: FontWeight.bold)),
+          TextSpan(
+              text:
+                  '${widget.aufgaben[random.nextInt(34)]}\n', //Anzahl der Aufgaben.
+              style: const TextStyle(fontSize: 30.0, color: Colors.black)),
+        ],
+      ),
+      textAlign: TextAlign.center,
+      style: const TextStyle(fontSize: 20.0, fontFamily: 'Karla'),
+    );
   }
 }
 
